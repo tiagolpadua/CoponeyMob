cd47e4c906773a5e85e85505a5de623ed4f07ded
tiago <tiagolpadua@gmail.com>
2018-11-04 14:35:28 -0200

    redux 0

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,10 +2,44 @@ import React from 'react';
 import { Button, Text, View } from 'react-native';
 
 export class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count += 1
+    });
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count -= 1
+    });
+  }
+
+  reset = () => {
+    this.setState({
+      count: 0
+    });
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Text>Contador: {this.state.count}</Text>
+        <Button
+          title="Add"
+          onPress={() => this.add()}
+        />
+        <Button
+          title="Remove"
+          onPress={() => this.remove()}
+        />
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
