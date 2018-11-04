f63da106f196026f0e16aba72c5412d6eb22dbdf
tiago <tiagolpadua@gmail.com>
2018-11-04 13:00:13 -0200

    superagent

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import Reactotron from 'reactotron-react-native';
 import { createStackNavigator } from 'react-navigation';
 import { HomeScreen } from './components/HomeScreen';
 import { DetailsScreen } from './components/DetailsScreen';
+import request from 'superagent';
 
+request
+  .get('https://cat-fact.herokuapp.com/facts')
+  .then(res => {
+    if(res.ok) {
+      console.log('Request realizado com sucesso!');
+    }
+  });
+  
 Reactotron.log('Testando a conexão com o Reactotron.');
 const RootStack = createStackNavigator(
   {
