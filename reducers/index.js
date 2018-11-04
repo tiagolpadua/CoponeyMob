201d8a599684135bd235212ade98955afcf26338
tiago <tiagolpadua@gmail.com>
2018-11-04 21:47:12 -0200

    home screen

--- reducers/index.js
+++ reducers/index.js
@@ -1,45 +1,32 @@
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-const RESET = 'RESET';
-const SET = 'SET';
+let poneys = [
+  { nome: 'Tremor', cor: 'Preto' },
+  { nome: 'Tzar', cor: 'Branco' },
+  { nome: 'Pégaso', cor: 'Cinza' },
+  { nome: 'Relâmpago', cor: 'Malhado' },
+  { nome: 'Tremor', cor: 'Preto' },
+  { nome: 'Tzar', cor: 'Branco' },
+  { nome: 'Pégaso', cor: 'Cinza' },
+  { nome: 'Relâmpago', cor: 'Malhado' },
+  { nome: 'Tremor', cor: 'Preto' },
+  { nome: 'Tzar', cor: 'Branco' },
+  { nome: 'Pégaso', cor: 'Cinza' },
+  { nome: 'Relâmpago', cor: 'Malhado' },
+  { nome: 'Tremor', cor: 'Preto' },
+  { nome: 'Tzar', cor: 'Branco' },
+  { nome: 'Pégaso', cor: 'Cinza' },
+  { nome: 'Relâmpago', cor: 'Malhado' },
+  { nome: 'Tremor', cor: 'Preto' },
+  { nome: 'Tzar', cor: 'Branco' },
+  { nome: 'Pégaso', cor: 'Cinza' },
+  { nome: 'Relâmpago', cor: 'Malhado' }
+];
 
-export default function reducer(state = { counter : 0 }, action) {
+poneys = poneys.map((p, idx) => ({ ...p, key: idx + '' }));
+
+export default function reducer(state = { poneys : poneys }, action) {
     console.log('Action: ' + action.type);
     switch (action.type) {
-      case INCREMENT:
-        return { ...state, counter: state.counter + 1 };
-      case DECREMENT:
-        return { ...state, counter: state.counter - 1 };
-      case RESET:
-        return { ...state, counter: 0 };
-      case SET:
-        return { ...state, counter: action.amount };
       default:
         return state;
     }
-}
-
-export function increment() {
-  return {
-    type: INCREMENT
-  }
-}
-
-export function decrement() {
-  return {
-    type: DECREMENT
-  }
-}
-
-export function reset() {
-  return {
-    type: RESET
-  }
-}
-
-export function set(amount) {
-  return {
-    type: SET,
-    amount,
-  }
 }
