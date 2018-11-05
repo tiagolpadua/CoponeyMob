6c66d3085fc82aa8e0d82ce4c2e7e347cb7c9879
tiago <tiagolpadua@gmail.com>
2018-11-04 22:20:22 -0200

    modal login

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,19 +1,69 @@
 import React from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Alert, TextInput, Modal, Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
 
 class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalVisible: false,
+      userName: 'admin',
+      password: '123456',
+      login: false
+    };
   }
-  
+
+  closeModal = () => {
+    this.setState({ modalVisible: false });
+  }
+
+  openModal = () => {
+    this.setState({ modalVisible: true });
+  }
+
+  login = () => {
+    this.closeModal();
+    if (this.state.userName === 'admin' && this.state.password === '123456') {
+      this.setState({ login: true });
+    } else {
+      Alert.alert(
+        'Erro',
+        'Credenciais inválidas',
+        [
+          { text: 'OK' },
+        ]
+      )
+    }
+  }
+
+  logoff = () => {
+    this.setState({
+      userName: 'admin',
+      password: '123456',
+      login: false
+    });
+  }
+
+  getUser = () => {
+    if (this.state.login) {
+      return (
+        <View>
+          <Text>Usuário Logado: {this.state.userName}</Text>
+          <Button title="Logoff" onPress={this.logoff} />
+        </View>
+      );
+    } else {
+      return <Button title="Logar" onPress={this.openModal} />
+    }
+  }
+
   render() {
     const { poneys } = this.props;
-    
+
     return (
       <View>
-        <Button title="Logar" onPress={() => null}/>
+        {this.getUser()}
         <FlatList
           data={poneys}
           renderItem={({ item }) => (
@@ -23,6 +73,27 @@ class HomeScreen extends React.Component {
             </View>
           )}
         />
+        <Modal
+          animationType="slide"
+          visible={this.state.modalVisible}
+          onRequestClose={this.closeModal}>
+          <View style={{ marginTop: 22 }}>
+            <View>
+              <TextInput
+                onChangeText={userName => this.setState({ userName })}
+                value={this.state.userName}
+              />
+              <TextInput
+                secureTextEntry={true}
+                onChangeText={password => this.setState({ password })}
+                value={this.state.password}
+              />
+
+              <Button title="Ok" onPress={this.login} />
+              <Button title="Cancelar" onPress={this.closeModal} />
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
