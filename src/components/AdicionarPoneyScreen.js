17dfd64e1a100eaf7ed4714b989f9dbfecd6b4dd
tiago <tiagolpadua@gmail.com>
2018-11-15 20:19:29 -0200

    form add ponei

--- src/components/AdicionarPoneyScreen.js
+++ src/components/AdicionarPoneyScreen.js
@@ -1,16 +1,32 @@
-import { Text } from "native-base";
+import PropTypes from "prop-types";
 import React from "react";
 import { connect } from "react-redux";
+import MantemPoneyForm from "./MantemPoneyForm";
 
 class AdicionaPoneyScreen extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  handleAddPoney = poney => {
+    alert("Ponei adicionado: " + JSON.stringify(poney));
+    this.props.navigation.navigate("ListarPoneys");
+  };
+
   render() {
-    return <Text>Tela de Inclusão de Poneis</Text>;
+    return (
+      <MantemPoneyForm
+        navigation={this.props.navigation}
+        onSubmit={this.handleAddPoney}
+      />
+    );
   }
 }
+
+AdicionaPoneyScreen.propTypes = {
+  addPoney: PropTypes.func,
+  navigation: PropTypes.object
+};
 
 const mapStateToProps = () => ({});
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import poneysReducer from "./poneys";
 import profileReducer from "./profile";
+import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
   profile: profileReducer,
-  poneys: poneysReducer
+  poneys: poneysReducer,
+  form: formReducer
 });
 
 export default rootReducer;
